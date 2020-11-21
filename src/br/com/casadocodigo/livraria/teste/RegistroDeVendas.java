fa1cdd3d636543f502d73c8c4eb8fa04da303e1e
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-11-21 11:46:33 -0300

    add exercicios de array entre as paginas 139 e 146

--- src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
+++ src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
@@ -3,6 +3,7 @@ package br.com.casadocodigo.livraria.teste;
 import br.com.casadocodigo.livraria.Autor;
 import br.com.casadocodigo.livraria.produtos.Ebook;
 import br.com.casadocodigo.livraria.produtos.LivroFisico;
+import br.com.casadocodigo.livraria.produtos.Produto;
 
 public class RegistroDeVendas {
 	public static void main(String[] args) {
@@ -24,5 +25,14 @@ public class RegistroDeVendas {
 		carrinho.adiciona(fisico);
 		carrinho.adiciona(ebook);
 		System.out.println("Total: " + carrinho.getTotal());
+		
+		Produto[] produtos = carrinho.getProdutos();
+		
+		for (int i = 0; i < produtos.length; i++) {
+			Produto produto = produtos[i];
+			if (produto != null) {
+				System.out.println(produto.getValor());
+			}
+		}
 	}
 }
