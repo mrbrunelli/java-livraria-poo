5534f3e24ad266da462ddc8aca272e5a9f2661ff
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-11-23 15:19:07 -0300

    add exercicios antre as paginas 192 e 203

---
+++ src/br/com/casadocodigo/livraria/GerenciadorDeCupons.java
@@ -0,0 +1,20 @@
+package br.com.casadocodigo.livraria;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GerenciadorDeCupons {
+	private Map<String, Double> cupons;
+	
+	public GerenciadorDeCupons() {
+		this.cupons = new HashMap<>();
+		cupons.put("cab11", 10.0);
+		cupons.put("cab22", 12.0);
+		cupons.put("cab33", 13.0);
+		cupons.put("cab44", 14.0);
+	}
+	
+	public Double validaCupom(String cupom) {
+		return this.cupons.get(cupom);
+	}
+}
